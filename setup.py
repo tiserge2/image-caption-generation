09a4851da364b58bf73ee1dc59a65dc38aae7bbf
Osson Sergio Suzerain <sergioosson@gmail.com>
2024-02-02 21:32:21 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Model for Remote Sensing Image Caption Generation',
+    author='Osson Sergio Suzerain',
+    license='MIT',
+)
